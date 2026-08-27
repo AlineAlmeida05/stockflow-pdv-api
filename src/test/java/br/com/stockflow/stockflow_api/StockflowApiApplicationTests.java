efe37bc10d41dd0cf9b7d1aa0fc9291d991b060f
Aline Melissa Andrade De Almeida <aline.almeida@trilliab3.com.br>
2026-08-27 13:34:31 -0300

    Projeto inicial Spring Boot

---
+++ src/test/java/br/com/stockflow/stockflow_api/StockflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.stockflow.stockflow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
